fa8096f281d6899f84d5975d4148a3e1d9a7afe8
ugsk\NikonovAV <NikonovAV@ugsk.ru>
2015-04-10 10:02:06 +0500

    добавлены все файлы. Проект не рабочий

---
+++ Blowfish.Test/BlowfishTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NikonovAV.HM.BlowfishCrypt;
+
+namespace NikonovAV.HM.BlowfishCrypt.Test
+{
+    [TestClass]
+    public class BlowfishTest
+    {
+        [TestMethod]
+        public void EncryptDecryptBlock_NonCryptedKeys()
+        {
+            byte[] key = new byte[40];
+            var expectedLeft = 10U;
+            var expectedRight = 235U;
+            var blowFeistel = new BlowFeistel();
+            var context = new BlowfishContext(key);
+            UInt32 left = expectedLeft;
+            UInt32 right = expectedRight;
+            blowFeistel.BlowfishEncrypt(context, ref left, ref right);
+            blowFeistel.BlowfishDecrypt(context, ref left, ref right);
+            Assert.AreEqual(left, expectedLeft);
+            Assert.AreEqual(right, expectedRight);
+        }
+
+        [TestMethod]
+        public void EncryptDecryptBlock_CryptedKeys()
+        {
+            byte[] key = new byte[56];
+            var expectedLeft = 10U;
+            var expectedRight = 235U;
+            var blowFeistel = new BlowFeistel();
+            Random rnd = new Random();
+            rnd.NextBytes(key);
+            var context = new BlowfishContext(key);
+            UInt32 left = expectedLeft;
+            UInt32 right = expectedRight;
+            blowFeistel.CryptPkeys(context);
+            blowFeistel.BlowfishEncrypt(context, ref left, ref right);
+            blowFeistel.BlowfishDecrypt(context, ref left, ref right);
+            Assert.AreEqual(left, expectedLeft);
+            Assert.AreEqual(right, expectedRight);
+        }
+
+        [TestMethod]
+        public void EncryptDecryptArray()
+        {
+            int dataLength = 13107200;
+            ulong[] dataArray = new ulong[dataLength];
+            ulong[] originalDataArray = new ulong[dataLength];
+            int keyLength = 56;
+            byte[] key = new byte[keyLength];
+
+            Random rnd = new Random();
+            rnd.NextBytes(key);
+            for(var i =0;i<dataLength;i++)
+            {
+                uint r = (uint)rnd.Next();
+                ulong l = (uint)rnd.Next();
+                dataArray[i] = (l << 32) | r;
+                originalDataArray[i] = dataArray[i];
+            }
+
+            Blowfish blowfish = new Blowfish(key);
+
+            blowfish.EncryptArray(dataArray);
+
+            blowfish.DecryptArray(dataArray);
+
+            for (var i = 0; i < dataLength; i++)
+            {
+                Assert.IsTrue(dataArray[i] == originalDataArray[i]);
+            }
+        }
+    }
+}
---
+++ Blowfish/Blowfish.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikonovAV.HM.BlowfishCrypt
+{
+    public class Blowfish
+    {
+        BlowfishContext context;
+        BlowFeistel blowFeistel;
+
+        public Blowfish(byte[] key)
+        {
+            context = new BlowfishContext(key);
+            blowFeistel = new BlowFeistel();
+            blowFeistel.CryptPkeys(context);
+        }
+
+        public ulong[] EncryptArray(ulong[] data)
+        {
+            long length = data.LongLength;
+            ulong[] result = new ulong[length];
+            for (long i = 0; i < length; i++)
+            {
+                UInt64 block = data[i];
+                uint right = (uint)(block & 0x00000000FFFFFFFF);
+                uint left = (uint)((block >> 32) & 0x00000000FFFFFFFF);
+                blowFeistel.BlowfishEncrypt(context, ref left, ref right);
+                ulong ecrypted = (ulong)left;
+                data[i] = (ecrypted << 32) | right;
+            }
+            return result;
+        }
+
+        public ulong[] DecryptArray(ulong[] data)
+        {
+            long length = data.LongLength;
+            ulong[] result = new ulong[length];
+            for (long i = 0; i < length; i++)
+            {
+                UInt64 block = data[i];
+                uint right = (uint)(block & 0x00000000FFFFFFFF);
+                uint left = (uint)((block >> 32) & 0x00000000FFFFFFFF);
+                blowFeistel.BlowfishDecrypt(context, ref left, ref right);
+                ulong decrypted = (ulong)left;
+                data[i] = (decrypted << 32) | right;
+            }
+            return result;
+        }
+    }
+}
