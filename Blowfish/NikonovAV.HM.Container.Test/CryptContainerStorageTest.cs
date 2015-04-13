0192a51f5ebd1084fec6800f94497aae48f396b5
Aleksandr Vladimirovich <alex_basic@mail.ru>
2015-04-13 16:41:39 +0500

    Добавлен проект виртуальной файловой системы

---
+++ Blowfish/NikonovAV.HM.Container.Test/CryptContainerStorageTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikonovAV.HM.Container.Test
+{
+    [TestClass]
+    public class CryptContainerStorageTest
+    {
+        [TestMethod]
+        public void Method1() 
+        {
+            MemoryStream original = null;
+            MemoryStream inconteiner = null;
+            CryptContainerStorage cryptContainerStorage = new CryptContainerStorage(original, inconteiner);
+
+            cryptContainerStorage.Pack();
+        }
+    }
+}
